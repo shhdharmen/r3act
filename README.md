e58e5c183e145b3401234839b6b2ea5cdc9f1b75
Dharmen Shah <dharmen@quarkstudios.com>
2019-09-05 15:55:32 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# create-project
+
+## Getting Started
+
+```bash
+git clone https://github.com/shhdharmen/create-project.git
+cd create-project
+npm i
+npm link
+```
+
+Then run `create-project` in project directory.
+
+## Ref
+
+https://www.twilio.com/blog/how-to-build-a-cli-with-node-js
---
+++ src/cli.js
@@ -0,0 +1,84 @@
+import arg from "arg";
+import inquirer from "inquirer";
+import { createProject } from "./main";
+
+function parseArgumentsIntoOptions(rawArgs) {
+  const args = arg(
+    {
+      "--git": Boolean,
+      "--yes": Boolean,
+      "--install": Boolean,
+      "-g": "--git",
+      "-y": "--yes",
+      "-i": "--install"
+    },
+    {
+      argv: rawArgs.slice(2)
+    }
+  );
+  return {
+    skipPrompts: args["--yes"] || false,
+    git: args["--git"] || false,
+    template: args._[0],
+    runInstall: args["--install"] || false
+  };
+}
+
+async function promptForMissingOptions(options) {
+  const defaultOptions = {
+    git: true,
+    template: "Javascript",
+    runInstall: true
+  };
+  if (options.skipPrompts) {
+    return {
+      ...options,
+      git: options.git || defaultOptions.git,
+      template: options.template || defaultOptions.template,
+      runInstall: options.runInstall || defaultOptions.runInstall
+    };
+  } else {
+    const questions = [];
+    if (!options.template) {
+      questions.push({
+        type: "list",
+        name: "template",
+        message: "Please choose which project template to use",
+        choices: ["JavaScript", "TypeScript"],
+        default: defaultOptions.template
+      });
+    }
+
+    if (!options.git) {
+      questions.push({
+        type: "confirm",
+        name: "git",
+        message: "Initialize a git repository?",
+        default: defaultOptions.git
+      });
+    }
+
+    if (!options.runInstall) {
+      questions.push({
+        type: "confirm",
+        name: "runInstall",
+        message: "Install dependencies?",
+        default: defaultOptions.runInstall
+      });
+    }
+
+    const answers = await inquirer.prompt(questions);
+    return {
+      ...options,
+      template: options.template || answers.template,
+      git: options.git || answers.git,
+      runInstall: options.runInstall || answers.runInstall
+    };
+  }
+}
+
+export async function cli(args) {
+  let options = parseArgumentsIntoOptions(args);
+  options = await promptForMissingOptions(options);
+  await createProject(options);
+}
